aa333b639d3f5a1dbf4965c455c6026d89540576
Josh Wasserman <github email address>
2018-03-28 15:39:33 -0400

    Changed Player and Bullet skins.

--- Frontend/src/bullet.js
+++ Frontend/src/bullet.js
@@ -1,37 +1,37 @@
-BULLETWIDTH = '30px';
-BULLETHEIGHT = '30px';
+BULLETWIDTH = "30px";
+BULLETHEIGHT = "30px";
 BULLETSARRAY = [];
 BULLETINTERVALS = [];
 class Bullet {
-
-  static clearBullets(){
+  static clearBullets() {
     BULLETSARRAY.forEach(bullet => bullet.remove());
     BULLETSARRAY.length = 0;
     BULLETINTERVALS.forEach(interval => clearInterval(interval));
     BULLETINTERVALS.length = 0;
   }
 
-  static createBulletDiv(){
-    const PLAYER = document.getElementById('player')
-    const bullet = document.createElement('div')
+  static createBulletDiv() {
+    const PLAYER = document.getElementById("player");
+    const bullet = document.createElement("div");
     bullet.style.width = BULLETWIDTH;
     bullet.style.height = BULLETHEIGHT;
-    bullet.className = 'bullet'
-    bullet.style.left = PLAYER.style.left
-    bullet.style.bottom = '30px'
-    bullet.innerHTML = '<img src="https://cdn.shopify.com/s/files/1/1061/1924/files/Middle_Finger_Emoji.png?9898922749706957214" style="width: 100%; height: 100%">'
+    bullet.className = "bullet";
+    bullet.style.left = PLAYER.style.left;
+    bullet.style.bottom = "30px";
+    bullet.innerHTML =
+      '<img src="https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_large.png?v=1513336434" style="width: 100%; height: 100%">';
     BULLETSARRAY.push(bullet);
-    document.getElementById('game').appendChild(bullet)
+    document.getElementById("game").appendChild(bullet);
 
-    function moveBulletUp(){
-      let bottomNumbers = bullet.style.bottom.replace('px','')
-      let bottom = parseInt(bottomNumbers, 10)
+    function moveBulletUp() {
+      let bottomNumbers = bullet.style.bottom.replace("px", "");
+      let bottom = parseInt(bottomNumbers, 10);
 
       if (bottom > 650) {
-        bullet.remove()
-        clearInterval(bulletInterval)
+        bullet.remove();
+        clearInterval(bulletInterval);
       } else {
-        bullet.style.bottom = `${bottom + 10}px`
+        bullet.style.bottom = `${bottom + 10}px`;
       }
       Bullet.checkCollision(bullet, bulletInterval);
     }
@@ -39,105 +39,129 @@ class Bullet {
     BULLETINTERVALS.push(bulletInterval);
   }
 
-  static checkCollision(bullet, bulletInterval){
-    let bottomNumbers = bullet.style.bottom.replace('px','')
-    let bottom = parseInt(bottomNumbers, 10)
-    let PLAYER = document.getElementById('player')
-    let BOX1 = document.getElementById('box1')
-    let BOX2 = document.getElementById('box2')
-    let BOX3 = document.getElementById('box3')
-    let BOSS = document.getElementById('boss')
-    let bulletLeft = Bullet.positionToInteger(bullet.style.left)
-    let bulletRight = Bullet.positionToInteger(bullet.style.left) + Bullet.positionToInteger(BULLETWIDTH)
+  static checkCollision(bullet, bulletInterval) {
+    let bottomNumbers = bullet.style.bottom.replace("px", "");
+    let bottom = parseInt(bottomNumbers, 10);
+    let PLAYER = document.getElementById("player");
+    let BOX1 = document.getElementById("box1");
+    let BOX2 = document.getElementById("box2");
+    let BOX3 = document.getElementById("box3");
+    let BOSS = document.getElementById("boss");
+    let bulletLeft = Bullet.positionToInteger(bullet.style.left);
+    let bulletRight =
+      Bullet.positionToInteger(bullet.style.left) +
+      Bullet.positionToInteger(BULLETWIDTH);
 
     if (BOX1) {
-      var box1Left = BOX1.offsetLeft
-      var box1Right = BOX1.offsetLeft + 100
+      var box1Left = BOX1.offsetLeft;
+      var box1Right = BOX1.offsetLeft + 100;
     }
     if (BOX2) {
-      var box2Left = BOX2.offsetLeft
-      var box2Right = BOX2.offsetLeft + 100
+      var box2Left = BOX2.offsetLeft;
+      var box2Right = BOX2.offsetLeft + 100;
     }
     if (BOX3) {
-      var box3Left = BOX3.offsetLeft
-      var box3Right = BOX3.offsetLeft + 100
+      var box3Left = BOX3.offsetLeft;
+      var box3Right = BOX3.offsetLeft + 100;
     }
-    if (BOSS){
-      var bossLeft = BOSS.offsetLeft
-      var bossRight = BOSS.offsetLeft + BOSSWIDTH
+    if (BOSS) {
+      var bossLeft = BOSS.offsetLeft;
+      var bossRight = BOSS.offsetLeft + BOSSWIDTH;
     }
 
-    let boxBottom = 325
+    let boxBottom = 325;
 
-    if (bulletLeft <= box1Left && bulletRight > box1Left && bottom >= 305||
-      bulletLeft >= box1Left && bulletRight <= box1Right && bottom >= 305||
-      bulletLeft <= box1Right && bulletRight >= box1Right && bottom >= 305){
-        clearInterval(bulletInterval)
-        setTimeout(function () {
-          bullet.remove()
-          box1.innerHTML = Item.render();
-          box2.remove()
-          box3.remove()
-          Bullet.clearBullets();
-          setTimeout(function () {
-            alert(`${Player.backpack[Player.backpack.length - 1].name}! ${Player.backpack[Player.backpack.length - 1].description}`)
-            if (Player.ammo === 0){
-              Player.status = "loss"
-              Game.endGame()
-            }else{
-              box1.remove();
-              Boss.createBoss();
-            }
-          }, 1000);
-        }, 10);
-
-    } else if (bulletLeft <= box2Left && bulletRight > box2Left && bottom >= 305||
-      bulletLeft >= box2Left && bulletRight <= box2Right && bottom >= 305||
-      bulletLeft <= box2Right && bulletRight >= box2Right && bottom >= 305) {
-        clearInterval(bulletInterval)
-        setTimeout(function () {
-          bullet.remove()
-          box2.innerHTML = Item.render();
-          box1.remove()
-          box3.remove()
-          Bullet.clearBullets();
-          setTimeout(function () {
-            alert(`${Player.backpack[Player.backpack.length - 1].name}! ${Player.backpack[Player.backpack.length - 1].description}`)
-            box2.remove();
+    if (
+      (bulletLeft <= box1Left && bulletRight > box1Left && bottom >= 305) ||
+      (bulletLeft >= box1Left && bulletRight <= box1Right && bottom >= 305) ||
+      (bulletLeft <= box1Right && bulletRight >= box1Right && bottom >= 305)
+    ) {
+      clearInterval(bulletInterval);
+      setTimeout(function() {
+        bullet.remove();
+        box1.innerHTML = Item.render();
+        box2.remove();
+        box3.remove();
+        Bullet.clearBullets();
+        setTimeout(function() {
+          alert(
+            `${Player.backpack[Player.backpack.length - 1].name}! ${
+              Player.backpack[Player.backpack.length - 1].description
+            }`
+          );
+          if (Player.ammo === 0) {
+            Player.status = "loss";
+            Game.endGame();
+          } else {
+            box1.remove();
             Boss.createBoss();
-          }, 1000);
-        }, 10);
-    }else if (bulletLeft <= box3Left && bulletRight > box3Left && bottom >= 305||
-      bulletLeft >= box3Left && bulletRight <= box3Right && bottom >= 305||
-      bulletLeft <= box3Right && bulletRight >= box3Right && bottom >= 305) {
-        clearInterval(bulletInterval)
-        setTimeout(function () {
-          bullet.remove()
-          box3.innerHTML = Item.render();
-          box1.remove();
+          }
+        }, 1000);
+      }, 10);
+    } else if (
+      (bulletLeft <= box2Left && bulletRight > box2Left && bottom >= 305) ||
+      (bulletLeft >= box2Left && bulletRight <= box2Right && bottom >= 305) ||
+      (bulletLeft <= box2Right && bulletRight >= box2Right && bottom >= 305)
+    ) {
+      clearInterval(bulletInterval);
+      setTimeout(function() {
+        bullet.remove();
+        box2.innerHTML = Item.render();
+        box1.remove();
+        box3.remove();
+        Bullet.clearBullets();
+        setTimeout(function() {
+          alert(
+            `${Player.backpack[Player.backpack.length - 1].name}! ${
+              Player.backpack[Player.backpack.length - 1].description
+            }`
+          );
           box2.remove();
-          Bullet.clearBullets();
-          setTimeout(function () {
-            alert(`${Player.backpack[Player.backpack.length - 1].name}! ${Player.backpack[Player.backpack.length - 1].description}`)
-            box3.remove();
-            Boss.createBoss();
-          }, 1000);
-          }, 10);
-    }else if (bulletLeft <= bossLeft && bulletRight > bossLeft && bottom >= BOSSBOTTOM||
-      bulletLeft >= bossLeft && bulletRight <= bossRight && bottom >= BOSSBOTTOM||
-      bulletLeft <= bossRight && bulletRight >= bossRight && bottom >= BOSSBOTTOM) {
-        clearInterval(bulletInterval)
-        setTimeout(function () {
-          bullet.remove();
-          Boss.takeHit();
-        }, 10);
-      }
-
-
+          Boss.createBoss();
+        }, 1000);
+      }, 10);
+    } else if (
+      (bulletLeft <= box3Left && bulletRight > box3Left && bottom >= 305) ||
+      (bulletLeft >= box3Left && bulletRight <= box3Right && bottom >= 305) ||
+      (bulletLeft <= box3Right && bulletRight >= box3Right && bottom >= 305)
+    ) {
+      clearInterval(bulletInterval);
+      setTimeout(function() {
+        bullet.remove();
+        box3.innerHTML = Item.render();
+        box1.remove();
+        box2.remove();
+        Bullet.clearBullets();
+        setTimeout(function() {
+          alert(
+            `${Player.backpack[Player.backpack.length - 1].name}! ${
+              Player.backpack[Player.backpack.length - 1].description
+            }`
+          );
+          box3.remove();
+          Boss.createBoss();
+        }, 1000);
+      }, 10);
+    } else if (
+      (bulletLeft <= bossLeft &&
+        bulletRight > bossLeft &&
+        bottom >= BOSSBOTTOM) ||
+      (bulletLeft >= bossLeft &&
+        bulletRight <= bossRight &&
+        bottom >= BOSSBOTTOM) ||
+      (bulletLeft <= bossRight &&
+        bulletRight >= bossRight &&
+        bottom >= BOSSBOTTOM)
+    ) {
+      clearInterval(bulletInterval);
+      setTimeout(function() {
+        bullet.remove();
+        Boss.takeHit();
+      }, 10);
+    }
   }
 
   static positionToInteger(p) {
-    return parseInt(p.split('px')[0]) || 0
+    return parseInt(p.split("px")[0]) || 0;
   }
-
 }
